03390ce38b2c5f9b9fc6e5ef80d43820da6cb08e
mbellange <mbellange@gmail.com>
2019-07-25 22:29:12 +0200

    add fullscreen function

--- client/dashboard/dashboard.js
+++ client/dashboard/dashboard.js
@@ -11,9 +11,28 @@ export class Dashboard extends React.Component {
     cards: PropTypes.object.isRequired
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      smallScreen: !document.fullscreenElement
+    }
+    document.addEventListener('fullscreenchange', () => {
+      if (document.fullscreenElement) {
+        this.setState({ smallScreen: false });
+      } else {
+        this.setState({ smallScreen: true });
+      }
+    });
+  }
+
   render() {
     return (
       <div className="dashboard">
+        <div className="full-screen" onClick={() => document.body.requestFullscreen()}>
+          {
+            this.state.smallScreen ? <img src="/images/pixel-full-screen.png" /> : ''
+          }
+        </div>
         {
           map(this.props.cards, (cardLink, index) =>
             <CardLink key={ index } cardLink={ cardLink } showDesc={true}></CardLink>)
