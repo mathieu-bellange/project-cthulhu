a07658348125fe992b4f88245bb29889a1f4beed
mbellange <mbellange@gmail.com>
2019-09-05 14:23:46 +0200

    end refacto redux

--- server/data/pnjs.js
+++ server/data/pnjs.js
@@ -1,7 +1,15 @@
 const WalterCorbitt = require('./walter-corbitt');
+const EliasLigget = require('./elias-ligget');
+const GabriellaMacario = require('./gabriella-macario');
+const VittorioMacario = require('./vittorio-macario');
+const DocteurHardstom = require('./docteur-hardstom');
 
 const Pnjs = {
-  'walter-corbitt': WalterCorbitt
+  'walter-corbitt': WalterCorbitt,
+  'elias-ligget': EliasLigget,
+  'gabriella-macario': GabriellaMacario,
+  'vittorio-macario': VittorioMacario,
+  'docteur-hardstom': DocteurHardstom
 };
 
 module.exports = Pnjs;
